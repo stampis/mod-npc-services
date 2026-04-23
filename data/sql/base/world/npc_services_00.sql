4645b78d534f99ef8a86dd60ff4c209a2ce959f5
stampis <jim_axling@hotmail.com>
2026-04-23 22:46:18 +0200

    Merge

---
+++ data/sql/base/world/npc_services_00.sql
@@ -0,0 +1,25 @@
+SET
+@Entry      := 300000,
+@Model      := 24877,
+@Name       := "Cromi",
+@Title      := "Instance Reset";
+
+DELETE FROM `creature_template` WHERE `entry` = @Entry;
+INSERT INTO `creature_template` (`entry`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `rank`, `unit_class`, `unit_flags`, `type`, `type_flags`, `RegenHealth`, `flags_extra`, `ScriptName`) VALUES(@Entry, @Name, @Title, "Speak", 0, 80, 80, 35, 0, 1, 1.14286, 0, 1, 2, 7, 138936390, 1, 2, "mod_npc_services");
+UPDATE `creature_template` SET `npcflag`=`npcflag`|1, `flags_extra`=`flags_extra`|16777216 WHERE `entry`=@Entry;
+
+DELETE FROM `creature_template_model` where creatureid=@Entry;
+INSERT INTO `creature_template_model` (CreatureID, Idx, CreatureDisplayID, DisplayScale, Probability, VerifiedBuild) VALUES
+(@Entry, '0', @Model, '1', '1', NULL);
+
+DELETE FROM `creature` WHERE (`id1` = @Entry);
+INSERT INTO `creature` (`id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`, `CreateObject`, `Comment`) VALUES
+(@Entry, 0, 0, 571, 0, 0, 1, 1, 0, 5641.15, 671.146, 651.993, 1.67377, 300, 0, 0, 5342, 0, 0, 0, 0, 0, '', NULL, 0, NULL), -- Dalaran (Eventide)
+(@Entry, 0, 0, 571, 0, 0, 1, 1, 0, 5966, 630.046, 650.627, 4.8625, 300, 0, 0, 5342, 0, 0, 0, 0, 0, '', NULL, 0, NULL), -- Dalaran (Antonidas)
+(@Entry, 0, 0, 0, 0, 0, 1, 65536, 0, -8803.54, 670.785, 96.2018, 4.56429, 300, 0, 0, 5342, 0, 0, 0, 0, 0, '', NULL, 0, NULL), -- Stormwind (Auctionhouse)
+(@Entry, 0, 0, 0, 0, 0, 1, 1, 0, -4890.18, -983.204, 503.941, 4.76355, 300, 0, 0, 5342, 0, 0, 0, 0, 0, '', NULL, 0, NULL), -- Ironforge (Bank)
+(@Entry, 0, 0, 1, 0, 0, 1, 1, 0, 9929.9, 2506.91, 1318.11, 4.5686, 300, 0, 0, 5342, 0, 0, 0, 0, 0, '', NULL, 0, NULL), -- Darnassus (Bank)
+(@Entry, 0, 0, 1, 0, 0, 1, 1, 0, -7235.16, -3798.66, -1.13841, 5.83534, 300, 0, 0, 5342, 0, 0, 0, 0, 0, '', NULL, 0, NULL), -- Gadgetzan (Auctionhouse)
+(@Entry, 0, 0, 530, 0, 0, 1, 1, 0, -2004.8, 5352.89, -9.35103, 0.775923, 300, 0, 0, 5342, 0, 0, 0, 0, 0, '', NULL, 0, NULL), -- Shattrath (Scryer Bank)
+(@Entry, 0, 0, 530, 0, 0, 1, 1, 0, -1718.07, 5495.11, -9.79913, 3.31763, 300, 0, 0, 5342, 0, 0, 0, 0, 0, '', NULL, 0, NULL), -- Shattrath (Aldor Bank)
+(@Entry, 0, 0, 530, 0, 0, 1, 1, 0, -695.188, 2705.66, 94.4671, 4.01217, 300, 0, 0, 5342, 0, 0, 0, 0, 0, '', NULL, 0, NULL); -- Honor Hold (Inn)
--- src/npc_services.cpp
+++ src/npc_services.cpp
@@ -10,26 +10,12 @@
 
 bool NpcServices::OnGossipHello(Player* player, Creature* creature)
 {
-    // Restore Health and Mana
-    AddGossipItemFor(player, 10, "|TInterface\\icons\\Spell_Nature_Regenerate:40:40:-18|t Restore HP and MP", GOSSIP_SENDER_MAIN, 1);
     // Reset Instances
-    AddGossipItemFor(player, 10, "|TInterface\\icons\\Achievement_BG_winAB_underXminutes:40:40:-18|t Reset Instances", GOSSIP_SENDER_MAIN, 2);
-    // Reset Cooldowns
-    AddGossipItemFor(player, 10, "|TInterface\\icons\\SPELL_HOLY_BORROWEDTIME:40:40:-18|t Reset Cooldowns", GOSSIP_SENDER_MAIN, 3);
-    // Leave Combat
-    AddGossipItemFor(player, 10, "|TInterface\\icons\\Achievement_BG_AB_defendflags:40:40:-18|t Reset Combat", GOSSIP_SENDER_MAIN, 4);
-    // Remove Sickness
-    AddGossipItemFor(player, 10, "|TInterface\\icons\\Spell_Shadow_DeathScream:40:40:-18|t Remove Sickness", GOSSIP_SENDER_MAIN, 5);
-    // Repair Items
-    AddGossipItemFor(player, 10, "|TInterface\\icons\\INV_Hammer_24:40:40:-18|t Repair Items", GOSSIP_SENDER_MAIN, 6);
-    // Reset Talents
-    AddGossipItemFor(player, 10, "|TInterface\\icons\\Achievement_WorldEvent_Lunar:40:40:-18|t Reset Talents", GOSSIP_SENDER_MAIN, 7);
+    AddGossipItemFor(player, 10, "|TInterface\\icons\\Achievement_BG_winAB_underXminutes:40:40:-18|t Reset Instances", GOSSIP_SENDER_MAIN, 1);
     // Open Bank
-    AddGossipItemFor(player, 10, "|TInterface/Icons/INV_Misc_Bag_07:40:40:-18|t Bank", GOSSIP_SENDER_MAIN, 8);
+    AddGossipItemFor(player, 10, "|TInterface/Icons/INV_Misc_Bag_07:40:40:-18|t Bank", GOSSIP_SENDER_MAIN, 2);
     // Open Mailbox
-    AddGossipItemFor(player, 10, "|TInterface/Icons/INV_Letter_11:40:40:-18|t Mail", GOSSIP_SENDER_MAIN, 9);
-    // Learn Dualspec
-    AddGossipItemFor(player, 10, "|TInterface/Icons/achievement_general:40:40:-18|t Learn Dual Talents", GOSSIP_SENDER_MAIN, 10);
+    AddGossipItemFor(player, 10, "|TInterface/Icons/INV_Letter_11:40:40:-18|t Mail", GOSSIP_SENDER_MAIN, 3);
 
     SendGossipMenuFor(player, 1, creature->GetGUID());
     return true;
@@ -41,116 +27,59 @@ bool NpcServices::OnGossipSelect(Player* player, Creature* /*creature*/, uint32 
 
     switch (action)
     {
-        case 1: // Restore HP and MP
+        case 1: // Reset Instances
             CloseGossipMenuFor(player);
-            if (player->IsInCombat())
-            {
-                CloseGossipMenuFor(player);
-                ChatHandler(player->GetSession()).SendNotification("You are in combat!");
-                return false;
-            }
-            else if (player->getPowerType() == POWER_MANA)
-                player->SetPower(POWER_MANA, player->GetMaxPower(POWER_MANA));
+            uint32 diff = 2;
 
-            player->SetHealth(player->GetMaxHealth());
-            ChatHandler(player->GetSession()).SendNotification("|cffFFFF00NPC SERVICES \n |cffFFFFFFHP & MP succesfully restored!");
-            player->CastSpell(player, 31726);
-            break;
-
-        case 2: // Reset Instances
-            CloseGossipMenuFor(player);
-            for (uint8 i = 0; i < MAX_DIFFICULTY; ++i)
+            std::vector<Player *> members;
+            members.push_back(player);
+            
+            Group *group = player->GetGroup();
+            if (group)
             {
-                BoundInstancesMap const& m_boundInstances = sInstanceSaveMgr->PlayerGetBoundInstances(player->GetGUID(), Difficulty(i));
-                for (BoundInstancesMap::const_iterator itr = m_boundInstances.begin(); itr != m_boundInstances.end();)
+                for (GroupReference *itr = group->GetFirstMember(); itr != nullptr; itr = itr->next())
                 {
-                    if (itr->first != player->GetMapId())
+                    Player *member = itr->GetSource();
+                    if (member)
                     {
-                        sInstanceSaveMgr->PlayerUnbindInstance(player->GetGUID(), itr->first, Difficulty(i), true, player);
-                        itr = m_boundInstances.begin();
+                        members.push_back(member);
                     }
-                    else
-                        ++itr;
                 }
             }
 
-           ChatHandler(player->GetSession()).SendNotification("|cffFFFF00NPC SERVICES \n |cffFFFFFFInstances succesfully reseted!");
+            for (uint8 i = 0; i < diff; ++i)
+            {
+                for (uint8 j = 0; j < members.size(); ++j)
+                {
+                    Player *member = members[j];
+                    BoundInstancesMap const &m_boundInstances = sInstanceSaveMgr->PlayerGetBoundInstances(member->GetGUID(), Difficulty(i));
+                    for (BoundInstancesMap::const_iterator itr = m_boundInstances.begin(); itr != m_boundInstances.end();)
+                    {
+                        if (itr->first != member->GetMapId())
+                        {
+                            sInstanceSaveMgr->PlayerUnbindInstance(member->GetGUID(), itr->first, Difficulty(i), true, member);
+                            itr = m_boundInstances.begin();
+                        }
+                        else
+                            ++itr;
+                    }
+                }
+            }
+
+            ChatHandler(player->GetSession()).SendNotification("|cffFFFFFFInstances reseted!");
             player->CastSpell(player, 59908);
             return true;
             break;
 
-        case 3: // Reset Cooldowns
-            CloseGossipMenuFor(player);
-            if (player->IsInCombat())
-            {
-                CloseGossipMenuFor(player);
-                ChatHandler(player->GetSession()).SendNotification("You are in combat!");
-                return false;
-            }
-
-            player->RemoveAllSpellCooldown();
-            ChatHandler(player->GetSession()).SendNotification("|cffFFFF00NPC SERVICES \n |cffFFFFFFCooldowns succesfully reseted!");
-            player->CastSpell(player, 31726);
-            break;
-
-        case 4: // Leave Combat
-            CloseGossipMenuFor(player);
-            player->CombatStop();
-            ChatHandler(player->GetSession()).SendNotification("|cffFFFF00NPC SERVICES \n |cffFFFFFFCombat succesfully removed!");
-            player->CastSpell(player, 31726);
-            break;
-
-        case 5: // Remove Sickness
-            CloseGossipMenuFor(player);
-            if (player->HasAura(15007))
-                player->RemoveAura(15007);
-            ChatHandler(player->GetSession()).SendNotification("|cffFFFF00NPC SERVICES \n |cffFFFFFFSickness succesfully removed!");
-            player->CastSpell(player, 31726);
-            break;
-
-        case 6: // Repair Items
-            CloseGossipMenuFor(player);
-            player->DurabilityRepairAll(false, 0, false);
-            ChatHandler(player->GetSession()).SendNotification("|cffFFFF00NPC SERVICES \n |cffFFFFFFItems repaired succesfully!");
-            player->CastSpell(player, 31726);
-            break;
-
-        case 7: // Reset Talents
-            CloseGossipMenuFor(player);
-            player->resetTalents(true);
-            player->SendTalentsInfoData(false);
-            ChatHandler(player->GetSession()).SendNotification("|cffFFFF00NPC SERVICES \n |cffFFFFFFTalents reseted succesfully!");
-            player->CastSpell(player, 31726);
-            break;
-
-        case 8:	// BANK
+        case 2:	// BANK
             CloseGossipMenuFor(player);
             player->GetSession()->SendShowBank(player->GetGUID());
             break;
 
-        case 9: // MAIL
+        case 3: // MAIL
             CloseGossipMenuFor(player);
             player->GetSession()->SendShowMailBox(player->GetGUID());
             break;
-
-        case 10: // Learn Dual Talent Specialization
-            CloseGossipMenuFor(player);
-            if (player->IsInCombat())
-            {
-                CloseGossipMenuFor(player);
-                ChatHandler(player->GetSession()).SendNotification("You are in combat!");
-                return false;
-            }
-
-            player->learnSpell(63644);
-            player->CastSpell(player, 31726);
-            player->CastSpell(player, 63624);
-            player->learnSpell(63645);
-            player->UpdateSpecCount(2);
-            ChatHandler(player->GetSession()).SendNotification("|cffFFFF00NPC SERVICES \n |cffFFFFFFDual Talents Learned Succesfully!");
-            return true;
-            break;
-
     }
     return true;
 }
